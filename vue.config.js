3708852c9709cde78b26442703d4c33e220bee6f
rockyjane <wooood80@gmail.com>
2019-07-13 17:41:42 +0800

    feat(api): google sign in

--- vue.config.js
+++ vue.config.js
@@ -9,5 +9,9 @@ module.exports = {
             }),
         ]
     },
+    // devServer: {
+    //     open: process.platform === 'darwin',
+    //     https: process.env.HTTP_OVER === 'https',
+    // },
     publicPath: process.env.BASE_URL,   // 消除 github 提供的網頁伺服器所存在的路徑 bug
 }
