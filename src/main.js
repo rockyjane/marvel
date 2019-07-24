bbe65442d58f126fda8015fa7b2fa5ebd01910e7
rockyjane <wooood80@gmail.com>
2019-07-24 18:48:01 +0800

    feat(plugins): vue-paginate 頁碼 && vue-clamp 行數限制

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import VeeValidate from 'vee-validate'                  // 表格檢測
 import zhTW from 'vee-validate/dist/locale/zh_TW.js';   // 表格檢測繁中包
 import VuePhotoZoomPro from 'vue-photo-zoom-pro'        // 放大鏡
 import 'font-awesome/css/font-awesome.css'              // 免費 ICON
+import VuePaginate from 'vue-paginate'                  // 頁碼
 
 // myfile
 import App from './App.vue';
@@ -32,6 +33,7 @@ VeeValidate.Validator.localize('zhTW', zhTW);           // 表格檢測繁中包
 Vue.use(BootstrapVue);
 Vue.use(VueGAPI, apiConfig);
 Vue.use(VuePhotoZoomPro);
+Vue.use(VuePaginate)                                    // 頁碼
 Vue.config.productionTip = process.env.NODE_ENV !== 'production';
 
 new Vue({
